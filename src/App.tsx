6027dcb21e861f91eed1a9fa92224403c898e154
mariushor <marius.horghidan@yahoo.com>
2023-05-11 23:21:52 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <h1>INVOICE APP</h1>
+    </div>
+  );
+}
+
+export default App;
