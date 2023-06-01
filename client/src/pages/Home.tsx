67a339ef2c9171f93186b46bcc66d532802d2e2d
mariushor <marius.horghidan@yahoo.com>
2023-06-01 16:52:59 +0300

    increased home page font size on small screens

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,12 +3,12 @@ import { LinkButton } from "components";
 export const Home = (): React.JSX.Element => {
   return (
     <div className="flex grow flex-col items-center justify-center gap-3 text-center lg:col-span-11 lg:col-start-2 lg:row-span-5 lg:row-start-1">
-      <h1 className="text-4xl font-bold text-skin-base md:text-6xl lg:text-8xl">
+      <h1 className="text-4xl font-bold text-skin-base sm:text-5xl md:text-6xl lg:text-8xl">
         Create<span className="text-skin-accent">.</span>Store
         <span className="text-skin-accent">.</span>Edit
         <span className="text-skin-accent">.</span>
       </h1>
-      <p className="max-w-sm text-sm leading-6 text-skin-muted sm:text-base md:max-w-lg md:text-base lg:max-w-2xl lg:text-xl">
+      <p className="leading- max-w-sm text-base text-skin-muted md:max-w-lg md:text-lg lg:max-w-2xl lg:text-xl">
         Simplify your invoicing journey, effortlessly store and manage your
         Invoices and take control of your finances with{" "}
         <span className="text-lg font-bold text-skin-accent md:text-xl lg:text-3xl">
